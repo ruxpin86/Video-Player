b26cc4c963727b704845f9ec810cf40e3a14cae5
Ted G <61029718+ruxpin86@users.noreply.github.com>
2022-10-10 21:51:44 -0600

    getting JS variables defined and fixing styling

---
+++ scripts.js
@@ -0,0 +1,10 @@
+//Get elements from the DOM
+const player = document.querySelector(".player");
+const video = player.querySelector(".viewer");
+const progress = player.querySelector(".progress");
+const progressBar = player.querySelector(".progress__filled");
+const toggle = player.querySelector(".toggle");
+const skipButtons = player.querySelector("[data-skip]");
+const ranges = player.querySelector(".player__slider");
+
+//Functions
